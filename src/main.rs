6ecf65eaf196ba24b605c94388996ca1f67804c7
Your Name <868689+cameronelliott@users.noreply.github.com>
2022-11-12 02:54:26 -0800

    Add some sha256 on plain, and crypted text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use std::time::Instant;
+#[allow(unused_imports)]
+use std::{
+    env::{args, args_os},
+    time::Instant,
+};
 
-fn main() {
+use anyhow::Result;
+
+#[cfg(feature = "sha")]
+use sha2::{Digest, Sha256};
+
+fn main() -> Result<()> {
+    let _a = args().skip(1).collect::<Vec<String>>();
+
+    // assert!(a.len() == 1, "args: [sharun]");
+
     //let key = &[0u8; 200][..]; // DO NOT USE IT ON PRODUCTION
 
     // https://datatracker.ietf.org/doc/html/rfc7714#section-16.1.1
@@ -21,8 +34,8 @@ fn main() {
     println!("key = {:?}", key);
     println!("plain len = {:?}", plain.len());
 
-    //let policy = srtp::CryptoPolicy::aes_gcm_128_16_auth();
-    let policy = srtp::CryptoPolicy::aes_cm_128_hmac_sha1_80();
+    let policy = srtp::CryptoPolicy::aes_gcm_128_16_auth();
+    //let policy = srtp::CryptoPolicy::aes_cm_128_hmac_sha1_80();
     let mut session = srtp::Session::with_inbound_template(srtp::StreamPolicy {
         key: &key,
         rtp: policy,
@@ -31,24 +44,54 @@ fn main() {
     })
     .unwrap();
 
-    const N: usize = 1e5 as usize;
+    const N: usize = 1e7 as usize;
+
+    #[cfg(feature = "sha")]
+    let mut plaintxt_sha = Sha256::new();
+    #[cfg(feature = "sha")]
+    let mut crypted_sha = Sha256::new();
+
+    // write input message
+
+    let mut buf = plain.clone();
+
+    let plain_len = plain.len();
 
     let now = Instant::now();
     for x in 1..(N as usize) {
-        plain[2] = ((x >> 8) & 0xff) as u8;
-        plain[3] = (x & 0xff) as u8;
+        buf.resize(plain_len, 0);
+        buf.copy_from_slice(plain.as_slice());
 
-        // println!("plain  = {:?}", plain);
+        buf[2] = ((x >> 8) & 0xff) as u8;
+        buf[3] = (x & 0xff) as u8;
 
-        match session.protect(&mut plain) {
-            Err(err) => println!("Error unprotecting SRTP packet: {}", err),
-            Ok(_) => {}
-        }
+        #[cfg(feature = "sha")]
+        plaintxt_sha.update(&buf);
+
+        session.protect(&mut buf)?; // fixme is '?' adding overhead?
+
+        #[cfg(feature = "sha")]
+        crypted_sha.update(&plain);
     }
 
     let elapsed = now.elapsed();
-    let nbits = N * plain.len() * 8;
-    let secs = elapsed.as_secs_f64();
+
     println!("Elapsed: {:.1?}", elapsed);
-    println!("Throughput: {:.1?} Gbps", nbits as f64 / secs / 1e9);
+
+    #[cfg(not(feature = "sha"))]
+    {
+        let nbits = N * plain.len() * 8;
+        let secs = elapsed.as_secs_f64();
+        println!("Throughput: {:.1?} Gbps", nbits as f64 / secs / 1e9);
+    }
+
+    #[cfg(feature = "sha")]
+    {
+        let sha = hex::encode(plaintxt_sha.finalize());
+        println!("input sha {}", sha);
+        const SRTP_IN: &str = "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9";
+        assert_eq!(sha.as_str(), SRTP_IN);
+    };
+
+    Ok(())
 }
